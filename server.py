5712d87b8deda726fae5c50df8aa147f99273290
bethany <bethanylynnpro@gmail.com>
2023-09-08 11:13:57 -0700

    created hello world

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from jinja2 import StrictUndefined
+from model import *
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """view homepage"""
+
+    return "Hello, world!"
 
 if __name__ == "__main__":
     connect_to_db(app)
