ceb995fad220d3addb9ed9fae83f9979244601ef
bethany <bethanylynnpro@gmail.com>
2023-09-11 12:28:03 -0700

    not using node

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// config file for tailwind
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
