a0e48b5795c10f8453a58e43773997ace81c61da
bethany <bethanylynnpro@gmail.com>
2023-09-22 11:06:24 -0700

    added border styling and fade in to image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,18 @@ module.exports = {
     },
     variants: {},
     plugins: [],
+    extend: {
+        theme: {
+            transitionProperty: {
+                'opacity': 'opacity',
+            },
+            transitionDuration: {
+                '700': '700ms',
+                '1000': '1000ms', 
+            },
+            transitionTimingFunction: {
+                'ease-in-out': 'ease-in-out',
+            },
+        },
+    },
 };
